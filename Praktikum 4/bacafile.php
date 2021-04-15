158d33572734540cf031abc1fe6bb75ac786a783
Dixy-04 <71267358+Dixy-04@users.noreply.github.com>
2021-04-15 15:56:51 +0700

    Delete Praktikum 4 directory

--- Praktikum 4/bacafile.php
+++
@@ -1,6 +0,0 @@
-<?php
-$namaFile="myfile.txt";
-$myfile = fopen($namaFile, "r") or die("File tidak dapat dibuka!");
-echo fread($myfile,filesize($namaFile));
-fclose($myfile);
-?>
--- Praktikum 4/bacafile2.php
+++
@@ -1,8 +0,0 @@
-<?php
-$namaFile = "myfile.txt";
-$myfile = fopen($namaFile, "r") or die("Tidak bisa buka file!");
-while(!feof($myfile)) {
-echo fgets($myfile);
-}
-fclose($myfile);
-?>
