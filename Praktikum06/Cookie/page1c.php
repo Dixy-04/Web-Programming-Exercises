7b4c498ee7037b916d4405e2af2a41e1d910618e
Dixy-04 <71267358+Dixy-04@users.noreply.github.com>
2021-04-29 13:08:23 +0700

    Update page1c.php

--- Praktikum06/Cookie/page1c.php
+++ Praktikum06/Cookie/page1c.php
@@ -5,5 +5,5 @@ echo "<h1>Ini Page 1</h1>";
 echo "<p>Selamat datang ".$_COOKIE['namauser']."</p>";
     
 echo "<h2>Menu Utama</h2>";
-echo "<p><a href='page1c.php'>Page 1</a> | <a href='page2c.php'>Page 2</a> | <a href='page3c.php'>Page 3</a> | <a href='logout.php'>Logout</a></p>";
+echo "<p><a href='page1c.php'>Page 1</a> | <a href='page2c.php'>Page 2</a> | <a href='page3c.php'>Page 3</a> | <a href='logoutCookie.php'>Logout</a></p>";
 ?>
