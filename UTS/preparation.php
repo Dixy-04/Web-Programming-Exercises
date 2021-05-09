56a729c70725a19d81a5844dee8bd730f8530b78
Dixy-04 <71267358+Dixy-04@users.noreply.github.com>
2021-05-09 12:06:29 +0700

    Update preparation.php

--- UTS/preparation.php
+++ UTS/preparation.php
@@ -1,6 +1,6 @@
 <?php session_start();?>
 <!-- sapaan serta menganbil value nyawa dan skor -->
-Hello <?php echo $_COOKIE['Nama'];?>,semangat ya.. you can do it. Try your best!!<br>
+Hello <?php echo $_COOKIE['Nama'];?>,semangat ya.. you can do it. Do your best!!<br>
 Lives : <?php echo $_SESSION['nyawa']; ?> | Score : <?php echo $_SESSION['skor'] ?>
 
 <?php
@@ -16,6 +16,6 @@ setcookie("num2",$num2);
 <!-- untuk memulai permainan -->
 <form action="game.php" method="post">
     Berapakah hasil dari <?php echo $num1;?> + <?php echo $num2;?> = 
-    <input name="jawaban" type="text" required/>
+    <input name="jawaban" type="text" onkeypress="return event.charCode ==13 || event.charCode >= 48 && event.charCode <=57" required/>
     <input type="submit" value="Jawab">
 </form>
