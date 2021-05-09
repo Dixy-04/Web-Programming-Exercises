c808e3f5ea40204b828fe926ee813b2c74284af9
Dixy-04 <71267358+Dixy-04@users.noreply.github.com>
2021-05-09 12:07:18 +0700

    Update jwbSalah.php

--- UTS/jwbSalah.php
+++ UTS/jwbSalah.php
@@ -3,4 +3,4 @@
 
 Waduhh <?php echo $_COOKIE["Nama"]; ?>, sayang sekali jawaban Anda salah… tetap semangat ya !!!<br>
 Lives: <?php echo $_SESSION['nyawa']; ?> | Score: <?php echo $_SESSION['skor'];?><br>
-<a href="preparation.php" > Soal selanjutnya </a>
+<a href="preparation.php" > Lanjut! </a>
